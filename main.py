9f0ccdadd4e0e6912859d1763a28910f8ab2bdc2
desmondlake <viantdinh@gmail.com>
2019-12-30 21:03:11 +0000

    new branch

--- main.py
+++ main.py
@@ -3,12 +3,15 @@
 #comment2
 
 def main(msg):
-
+    // no longer need comments
     //fourth comment
 
     // Adding the second comment to this file
     // Adding third comment to this file
 
     print (msg)
+
+def secondary():
+    print "new function"
 
 main("Hello people!")
